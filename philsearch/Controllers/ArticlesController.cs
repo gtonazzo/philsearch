cd2d43cb3ee050967244c3260c6a928f55ee17b1
gio <g.tonazzo2@campus.unimib.it>
2018-03-17 22:49:05 +0100

    first

---
+++ philsearch/Controllers/ArticlesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using philsearch.Models;
+
+namespace philsearch.Controllers
+{
+    public class Search
+    {
+        public string search { get; set; }
+    }
+
+    public class ArticlesController : Controller
+    {
+        
+        public IActionResult Index(string searchText)
+        {
+            Articles result = new Articles();
+
+            if (!String.IsNullOrEmpty(searchText))
+            {
+                result = SearchArticles(searchText);
+            }
+
+            return View(result);
+        }
+
+
+        public string ArticlesJson(string searchText)
+        {
+            Articles result = SearchArticles(searchText);
+            return JsonConvert.SerializeObject(result, Formatting.Indented);
+        }
+
+        private Articles SearchArticles(string searchText)
+        {
+            Search s = new Search();
+            s.search = searchText;
+
+            ArticlesContext context = HttpContext.RequestServices.GetService(typeof(philsearch.Models.ArticlesContext)) as ArticlesContext;
+            string webService = context.SearchArticlesWS;
+
+            IEnumerable<SimilarArticles> artList;
+            using (HttpClient client = new HttpClient())
+            {
+                MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                string stringData = JsonConvert.SerializeObject(s);
+                var contentData = new StringContent(stringData, System.Text.Encoding.UTF8, "application/json");
+                HttpResponseMessage response = client.PostAsync(webService, contentData).Result;
+                artList = JsonConvert.DeserializeObject<IEnumerable<SimilarArticles>>(response.Content.ReadAsStringAsync().Result);
+
+            }
+
+            string filter = "";
+            foreach (SimilarArticles a in artList)
+            {
+                if (filter.Length > 0) { filter = filter + ","; }
+                filter = filter + "'" + a.Art_Id + "'";
+            }
+
+
+            List<Article> articles = context.GetArticles(filter);
+
+            Articles result = new Articles();
+            result.SimilarArticlesList = artList;
+            result.ArticlesList = articles;
+            result.ConceptsNetwork = GetSemanticNetwork(searchText);
+            return result;
+
+        }
+
+        public SemanticNetwork GetSemanticNetwork(string searchText)
+        {
+
+            SemanticNetwork semNet = new SemanticNetwork();
+
+            Search s = new Search();
+            s.search = searchText;
+
+            ArticlesContext context = HttpContext.RequestServices.GetService(typeof(philsearch.Models.ArticlesContext)) as ArticlesContext;
+            string webService = context.SemanticNetworkWS;
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                string stringData = JsonConvert.SerializeObject(s);
+                var contentData = new StringContent(stringData, System.Text.Encoding.UTF8, "application/json");
+                HttpResponseMessage response = client.PostAsync(webService, contentData).Result;   
+                semNet = JsonConvert.DeserializeObject<SemanticNetwork>(response.Content.ReadAsStringAsync().Result);
+
+            }
+
+            return semNet;
+
+        }
+    }
+}
---
+++ philsearch/Models/ArticlesContext.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using philsearch.Models;
+using System.Data;
+using System.Text;
+
+namespace philsearch.Models
+{
+    public class ArticlesContext
+    {
+
+        public string ConnectionString { get; set; }
+        public string SearchArticlesWS { get; set; }
+        public string SemanticNetworkWS { get; set; }
+
+        public ArticlesContext(string connectionString, string searchArticlesWS, string semanticNetworkWS)
+        {
+            this.ConnectionString = connectionString;
+            this.SearchArticlesWS = searchArticlesWS;
+            this.SemanticNetworkWS = semanticNetworkWS;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<Article> GetArticles(string filter)
+        {
+            List<Article> list = new List<Article>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from articles where art_id in (" + filter + ")", conn);
+
+                using (MySqlDataAdapter sda = new MySqlDataAdapter(cmd))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(new Article()
+                            {
+                                ArtId = reader["art_id"].ToString(),
+                                Title = reader["title"].ToString(),
+                                Abstract = reader["art_abstract"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+
+            foreach (Article a in list)
+            {
+                a.Authors = GetAuthors(a.ArtId);
+                a.Categories = GetCategories(a.ArtId);
+                a.Features = GetFeatures(a.ArtId);
+                a.References = GetReferences(a.ArtId);
+            };
+
+            return list;
+        }
+
+        public List<String> GetAuthors(string artId)
+        {
+            List<String> list = new List<String>() { };
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from articles_authors where art_id = '" + artId + "'", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(reader["author"].ToString());
+                    }
+                }
+            }
+            return list;
+        }
+
+        public List<String> GetCategories(string artId)
+        {
+            List<String> list = new List<String>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from articles_categories where art_id = '" + artId + "'", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(reader["category"].ToString());
+                    }
+                }
+            }
+            return list;
+        }
+
+        public List<Article.Reference> GetReferences(string artId)
+        {
+            List<Article.Reference> list = new List<Article.Reference>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("Select b.* from articles_biblio a inner join biblio b on a.biblio_id = b.biblio_id  where art_id = '" + artId + "'", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Article.Reference()
+                        {
+                            Id = reader["biblio_id"].ToString(),
+                            Year = reader["year"].ToString(),
+                            Author = reader["author"].ToString(),
+                            Title = reader["title"].ToString(),
+                            Journal = reader["journal"].ToString(),
+                            Number = reader["number"].ToString(),
+                            Volume = reader["volume"].ToString(),
+                            Pages = reader["pages"].ToString(),
+                            Publisher = reader["publisher"].ToString()
+
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+
+
+
+        public List<Article.Feature> GetFeatures(string artId)
+        {
+            List<Article.Feature> list = new List<Article.Feature>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from articles_features where art_id = '" + artId + "'", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Article.Feature()
+                        {
+                            Id = reader["feature"].ToString(),
+                            TfIdf = Convert.ToDouble(reader["tfidf"])
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+
+    }
+}
